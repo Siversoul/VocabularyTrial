0af7be8d39584980181d636ed1f85cbf35e770bf
Oliver Stiller <oliver.stiller@siemens.com>
2019-03-05 14:05:04 +0100

    Deployed version 0.3.1 and updated readme

--- src/com/visparu/vocabularytrial/root/Main.java
+++ src/com/visparu/vocabularytrial/root/Main.java
@@ -18,7 +18,7 @@ import javafx.stage.Stage;
 public final class Main extends Application
 {
 	public static final String	NAME			= I18N.createStringBinding("root.name").get();
-	public static final String	VERSION			= "0.3.0";
+	public static final String	VERSION			= "0.3.1";
 	public static final String	AUTHOR			= "Oliver Stiller";
 	public static final String	RELEASE_DATE	= "05.03.2019";
 	
